5075b9faf40f3b2896b83e45373c0577f82364a0
Yukino Hayakawa <tennencoll@gmail.com>
2025-05-07 02:49:09 +0800

    update project templates & packing script & add icons

--- Templates/CopyTemplates.ps1
+++ Templates/CopyTemplates.ps1
@@ -1,2 +1,6 @@
-.\PackTemplates.bat
-Copy-Item * ([Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments) + "\Visual Studio 2019\Templates\ProjectTemplates\Visual C++") -Filter "*.zip"
+# .\PackTemplates.bat
+# According to MSDN, the path to the Visual Studio 2022 templates is:
+# %USERPROFILE%\Documents\Visual Studio {Version}\Templates\ProjectTemplates
+# and they should be .zip files.
+# (See https://learn.microsoft.com/en-us/visualstudio/ide/how-to-create-project-templates?view=vs-2022)
+Copy-Item * ([Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments) + "\Visual Studio 2022\Templates\ProjectTemplates\") -Filter "*.zip"
---
+++ Templates/UsagiTest/main.cpp
@@ -0,0 +1,16 @@
+﻿#ifdef _DEBUG
+    #pragma comment(lib, "gtestd.lib")
+    // Replaced by <format>
+    // #pragma comment(lib, "fmtd.lib")
+#else
+    #pragma comment(lib, "gtest.lib")
+    // Replaced by <format>
+    // #pragma comment(lib, "fmt.lib")
+#endif
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
